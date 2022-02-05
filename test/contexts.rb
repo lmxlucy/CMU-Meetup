658d2465819ac161ef54eed7200388210eb72702
lmxlucy <56939921+lmxlucy@users.noreply.github.com>
2022-02-06 01:33:12 +0800

    add factory bot gem

--- test/contexts.rb
+++ test/contexts.rb
@@ -7,15 +7,15 @@ require './test/sets/event_users'
 
 module Contexts
   # explicitly include all sets of contexts used for testing 
-  include Contexts::Events
   include Contexts::Groups
+  include Contexts::Events
   include Contexts::Users
   include Contexts::EventUsers
 
   
   def create_all
-    create_events
     create_groups
+    create_events
     create_users
     create_event_users
   end
