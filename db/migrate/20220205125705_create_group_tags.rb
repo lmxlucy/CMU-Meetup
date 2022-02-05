956de4ebfbb04d3e02e9f2336ee85fc19b21bdd2
lmxlucy <56939921+lmxlucy@users.noreply.github.com>
2022-02-05 20:57:18 +0800

    group tag setup

---
+++ db/migrate/20220205125705_create_group_tags.rb
@@ -0,0 +1,10 @@
+class CreateGroupTags < ActiveRecord::Migration[5.2]
+  def change
+    create_table :group_tags do |t|
+      t.references :group, foreign_key: true
+      t.references :tag, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
